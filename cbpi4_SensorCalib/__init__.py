81e177415e4883cddad5dffad60420f286ad4e5a
mjiao5151 <meng1usa@gmail.com>
2024-05-28 15:21:11 +0800

    init

---
+++ cbpi4_SensorCalib/__init__.py
@@ -0,0 +1,69 @@
+
+# -*- coding: utf-8 -*-
+#import os
+#from aiohttp import web
+import logging
+#from unittest.mock import MagicMock, patch
+import asyncio
+from time import sleep
+#import random
+from cbpi.api import *
+from cbpi.api.base import CBPiBase
+from statistics import fmean
+
+logger = logging.getLogger(__name__)
+
+
+@parameters([Property.Sensor(label="Sensor01", description="Select a sensor for this group."),
+            Property.Number(label="a", description="as a in y= a*x^2 + b*x + c"),
+            Property.Number(label="b", description="as b in y= a*x^2 + b*x + c"),
+            Property.Number(label="c", description="as c in y= a*x^2 + b*x + c")])
+
+class GroupedSensor(CBPiSensor):
+
+
+    def __init__(self, cbpi, id, props):
+        super(GroupedSensor, self).__init__(cbpi, id, props)
+        self.value = 0      
+        self.sensors = 0
+        self.a = self.props.get("a", 0)
+        self.b = self.props.get("b", 0)
+        self.c = self.props.get("c", 0)
+
+        logging.info("GROUPED Sensor")
+        if self.props.get("Sensor01", None) is not None:
+            self.sensors = self.props.get("Sensor01")
+        
+        pass
+
+    def get_state(self):
+        return self.value
+   
+    async def run(self):
+        while self.running == True:
+            values = None
+            try:
+                
+                sensor_value = self.cbpi.sensor.get_sensor_value(self.sensors).get("value")
+                if sensor_value is not None:
+                    values = float(sensor_value)
+                
+                if values:
+                    self.value = self.a * values**2 + self.b * values + self.c
+                else:
+                    logging.info("No values fetched from the selected child sensors, check connections and setup")
+
+            except Exception as e:
+                logging.info(e)
+                pass 
+
+            self.log_data(self.value)
+            self.push_update(self.value)
+            await asyncio.sleep(1)
+
+        pass
+
+
+def setup(cbpi):
+    cbpi.plugin.register("Grouped Sensor", GroupedSensor)
+    pass
--- setup.py
+++ setup.py
@@ -6,18 +6,18 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='cbpi4_GroupedSensor',
-      version='0.0.4',
+setup(name='cbpi4_SensorCalib',
+      version='0.0.1',
       description='CraftBeerPi4 Sensor Plugin',
       author='A. Vollkopf, B. Helm',
-      author_email='avollkopf@web.de',
-      url='https://github.com/PiBrewing/cbpi4_GroupedSensor',
+      author_email='mjiao1purdue@qq.com',
+      url='',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
       '': ['*.txt', '*.rst', '*.yaml'],
-      'cbpi4_GroupedSensor': ['*','*.txt', '*.rst', '*.yaml']},
-      packages=['cbpi4_GroupedSensor'],
+      'cbpi4_SensorCalib': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4_SensorCalib'],
       long_description=long_description,
 	    long_description_content_type='text/markdown'
      )
